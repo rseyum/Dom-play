7bcbce7f25d8f353c4df5fb715d3dabe63b0eba3
rseyum <rseyum@sea.yearup.org>
2022-07-23 13:03:52 -0700

    fixes-firebase issue

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 //alert("test");
 
 //https://tinyurl.com/dynamic-html-checker
-document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+document
+  .getElementById("html-checker")
+  .setAttribute("href", "https://validator.w3.org/nu/?doc=" + location.href);
 
-document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
+document
+  .getElementById("css-checker")
+  .setAttribute(
+    "href",
+    "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href
+  );
 
-//manages mobile nav 
+//manages mobile nav
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-} 
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
